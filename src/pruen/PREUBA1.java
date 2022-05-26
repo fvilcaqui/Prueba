5e8f7153669830b9cb6b7ba998bab23c6ad87cd8
fvilcaqui <fvilcaqui@unsa.edu.pe>
2022-05-26 10:50:38 -0500

    Nuevo agregado

---
+++ src/pruen/PREUBA1.java
@@ -0,0 +1,12 @@
+package pruen;
+//Ejercicio1
+public class PREUBA1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Prueba");
+System.out.println("Ahora subiremos");
+int a=0;
+	}
+
+}
